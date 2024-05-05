f7ccd6fed8df9f30bbadbca8879bab8e6e5f44d0
mohammadreza salek <rezasalek75@gmail.com>
2024-05-06 00:12:35 +0330

    train for using callbacks

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log("Before");
-getUser(1, function (user) {
-  console.log("User", user);
+getUser(1, (user) => {
+  //   console.log("User", user);
+  //   Get the repositories
+  getRepositories(user.githubUserName, (repos) => {
+    console.log("repos", repos);
+  });
 });
 console.log("After");
 
@@ -8,5 +12,12 @@ function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database ...");
     callback({ id: id, githubUserName: "reza" });
+  }, 2000);
+}
+
+function getRepositories(username, cb) {
+  setTimeout(() => {
+    console.log("Calling github api ...");
+    cb(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
